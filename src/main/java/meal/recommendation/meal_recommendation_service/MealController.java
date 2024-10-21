ee4c63af84dfa6850d5900ad8da9a8da91ad090e
Dilip Rao <diliprao@Dilips-MBP.Dlink>
2024-10-21 15:55:24 +0530

    Make getRecipes type GET Handle wrong input

--- src/main/java/meal/recommendation/meal_recommendation_service/MealController.java
+++ src/main/java/meal/recommendation/meal_recommendation_service/MealController.java
@@ -1,14 +1,13 @@
 package meal.recommendation.meal_recommendation_service;
-import java.util.List;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
 import meal.recommendation.meal_recommendation_service.API.Responses.Recipe;
-import meal.recommendation.meal_recommendation_service.Requests.RecipeRequest;
 import meal.recommendation.meal_recommendation_service.services.RecipeService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @CrossOrigin(origins = "http://localhost:3000") 
 @RestController
@@ -21,13 +20,25 @@ public class MealController {
     }
 
 
-    @CrossOrigin(origins = "http://localhost:3000") 
-    @PostMapping("/api/recipes")
-    public List<Recipe> index(@RequestBody RecipeRequest recipeRequest) throws Exception{
-        String query = recipeRequest.getQuery();
-        String cuisine = recipeRequest.getCuisine();
-       
-        return  recipeService.fetch(query,cuisine);
+    @GetMapping("/api/recipes")
+    public List<Recipe> index(@RequestParam String mealType,@RequestParam String cuisine) throws Exception{
+        if (mealType == null || mealType.isEmpty()) {
+            throw new IllegalArgumentException("mealType is required");
+        }
+        if (cuisine == null || cuisine.isEmpty()) {
+            throw new IllegalArgumentException("cuisine is required");
+        }
+        return  recipeService.fetch(mealType,cuisine);
        // return "Greetings from Meal Recommendation Service";
     }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleIllegalArgumentException(IllegalArgumentException ex) {
+        Map<String, String> errorResponse = new HashMap<>();
+        errorResponse.put("error", "Invalid Request");
+        errorResponse.put("message", ex.getMessage());  // Exception message returned in JSON
+        return errorResponse;
+    }
+
 }
